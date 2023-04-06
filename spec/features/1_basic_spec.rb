ef37d2775dff8c79f143e3bc3e1a736984ace6e4
Ben Purinton <ben.purinton@gmail.com>
2023-04-06 00:56:16 +0000

    WIP

--- spec/features/1_basic_spec.rb
+++ spec/features/1_basic_spec.rb
@@ -1,7 +1,16 @@
 require "rails_helper"
 
-describe "This project" do
-  it "has no tests" do
-    expect(1).to eq(1)
+describe "/users/sign_in" do
+  let(:user) { User.create(username: "alice", email: "alice@example.com", password: "password") }
+
+  it "allows a user to sign in", points: 2 do
+    visit new_user_session_path
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully."),
+      "Expected to visit /users/sign_in, login with a signed up user, and be redirected to the homepage with a notification 'Signed in successfully.'."
   end
 end
